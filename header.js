063ceaf97a35d995bd84b1d38016c509e78aeb57
Nandiniatri <atrinandini8@gmail.com>
2023-12-23 21:28:23 +0530

    modified css

--- header.js
+++ header.js
@@ -1,13 +1,14 @@
 export class Header{
     constructor(data){ 
-        this.data = data;
+        this.data = data; 
     }
 
 renderHeader(){
     const tpl = this.data.map((item) =>{ 
         return `<div id="header-container">
+
         <div class="nandu">
-        <p>${item.nandini}</p>
+        <p class="header-pp">${item.nandini}</p>
         </div>
         <a href="#" id="menu-icon"><i style="font-size:35px" class="fa">&#xf0c9;</i></a>
         
